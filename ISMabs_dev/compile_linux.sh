eba087c11bd1292e0aeb9406194e26a7a084bd79
Efrain Gatuzz <efraingatuzz@gmail.com>
2020-12-10 15:11:25 +0100

    new atomic data

--- ISMabs_dev/compile_linux.sh
+++ ISMabs_dev/compile_linux.sh
@@ -5,11 +5,6 @@ rm lib*
 sed -i "s,local_dir = '.*',local_dir = \'`pwd`\'," ismabs_dev.f90  
 echo "initpackage ismabsdev lmodel_ismabs_dev.dat "`pwd`"\nquit\ny" | xspec
 
-# For Mac OSX
-#rm *.dylib
-#sed -i.ori "s,local_dir = '.*',local_dir = \'`pwd`\'," ismabs_dev.f90  
-#echo "initpackage ismabsdev lmodel_ismabs_dev.dat "`pwd`"\nquit\ny" | xspec
-
 rm *~ *.o
 rm *FunctionMap.* lpack_* 
 rm -f *.mod Makefile
---
+++ ISMabs_dev/compile_mac.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# For Mac OSX
+rm *.dylib
+sed -i.ori "s,local_dir = '.*',local_dir = \'`pwd`\'," ismabs_dev.f90  
+echo "initpackage ismabsdev lmodel_ismabs_dev.dat "`pwd`"\nquit\ny" | xspec
+
+rm *~ *.o
+rm *FunctionMap.* lpack_* 
+rm -f *.mod Makefile
