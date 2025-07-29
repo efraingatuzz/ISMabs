f225e69035ff52e12e1e343f48662f30ff576443
Efrain Gatuzz <efraingatuzz@gmail.com>
2025-07-29 16:11:48 +0200

    NewChanges

---
+++ ISMabs_individual_ions/ismabs.silicon.dev/compile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# For Linux
+rm lib*
+fullpath=$(pwd)
+
+part1=${fullpath:0:80}
+part2=${fullpath:80} 
+
+# Set local_dir1 and local_dir2 properly
+sed -i "s,local_dir1 = '.*',local_dir1 = '$part1'," ismabssilicon.f90
+sed -i "s,local_dir2 = '.*',local_dir2 = '$part2'," ismabssilicon.f90
+    
+echo "initpackage ismabssilicon lmodel_ismabs_silicon.dat "`pwd`"\nquit\ny" | xspec
+
+         
+            
+            
+# For Mac OSX
+#rm *.dylib
+#fullpath=$(pwd)
+#part1=${fullpath:0:80}
+#part2=${fullpath:80} 
+## Set local_dir1 and local_dir2 properly
+#sed -i.ori "s,local_dir1 = '.*',local_dir1 = '$part1'," ismabssilicon.f90
+#sed -i.ori "s,local_dir2 = '.*',local_dir2 = '$part2'," ismabssilicon.f90
+#echo "initpackage ismabssilicon lmodel_ismabs_silicon.dat "`pwd`"\nquit\ny" | xspec
+
+rm *~ *.o
+rm *FunctionMap.* lpack_* 
+rm -f *.mod Makefile
