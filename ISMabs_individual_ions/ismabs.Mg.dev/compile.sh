7a8b7605abee4f0c2bbbbbe56019e3323efb85ce
Efrain Gatuzz <efraingatuzz@gmail.com>
2025-08-05 18:21:09 +0200

    ChangedNames

---
+++ ISMabs_individual_ions/ismabs.Mg.dev/compile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# For Linux
+rm lib*
+fullpath=$(pwd)
+
+part1=${fullpath:0:80}
+part2=${fullpath:80} 
+
+# Set local_dir1 and local_dir2 properly
+sed -i "s,local_dir1 = '.*',local_dir1 = '$part1'," ismabsmg.f90
+sed -i "s,local_dir2 = '.*',local_dir2 = '$part2'," ismabsmg.f90
+    
+echo "initpackage ismabsmg lmodel_ismabsmg.dat "`pwd`"\nquit\ny" | xspec
+
+         
+            
+            
+# For Mac OSX
+#rm *.dylib
+#fullpath=$(pwd)
+#part1=${fullpath:0:80}
+#part2=${fullpath:80} 
+## Set local_dir1 and local_dir2 properly
+#sed -i.ori "s,local_dir1 = '.*',local_dir1 = '$part1'," ismabsmg.f90
+#sed -i.ori "s,local_dir2 = '.*',local_dir2 = '$part2'," ismabsmg.f90
+#echo "initpackage ismabsmg lmodel_ismabsmg.dat "`pwd`"\nquit\ny" | xspec
+
+rm *~ *.o
+rm *FunctionMap.* lpack_* 
+rm -f *.mod Makefile
