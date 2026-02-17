e97e3e2873a87c9e537316d58653eabe29786ea7
Efrain Gatuzz <efraingatuzz@gmail.com>
2026-02-17 13:10:58 +0100

    Update ismabsmg.f90 for XSPEC 12.15.1
    Updated version information and added 'use xsfortran' for XSPEC compatibility.

--- ISMabs_individual_ions/ismabs.Mg.dev/ismabsmg.f90
+++ ISMabs_individual_ions/ismabs.Mg.dev/ismabsmg.f90
@@ -2,6 +2,11 @@
 ! ismabsmg
 ! ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ! XSPEC local model for ISM absorption due to magnesium
+! Version 1.1 February 2026
+!
+! Additions to version 1.1
+! - We added the "use xsfortran" line to call all fortran routines included in xspec (valid for XSPEC 12.15.1)
+!
 ! Version 1.0 July 2025 
 !
 ! - This version only has CI-CVI as free parameters. 
@@ -65,7 +70,7 @@ subroutine read_cross_sections_ismabsmg(bnene, xs, ifl)
   ! Looks for the file at:
   !     <ismabsmg_root>/atomic_data/AtomicData.fits
   !---------------------------------------------------------------------
-
+  use xsfortran
   implicit none
   integer, parameter :: nion = 12, out_unit = 20
   integer, intent(in)  :: bnene, ifl
@@ -89,7 +94,7 @@ subroutine read_cross_sections_ismabsmg(bnene, xs, ifl)
 
   !--- Define data root location
   local_dir1 = '/media/efrain/DATA/softwares/modelosXSPEC/ismabs/ismabs_no_turb/ismabs_species/i'
-  local_dir2 = 'smabs.Mg.dev'
+  local_dir2 = 'smabs.Mg/v1.1'
   ismabsmg_root = trim(local_dir1) // trim(local_dir2)
   filename2 = trim(ismabsmg_root) // fileloc
   chatmsg = trim(ismreadchat) // trim(filename2)
